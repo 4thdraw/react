2f9211a6a5aa649b2882466162e4c3fdeef47d76
4thdraw <4thdraw2@gmail.com>
2023-12-11 12:42:20 +0900

    내생의 처음 리액트

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <>
+      <p>컴포넌트? 컨텐츠공용화로 자바스크립트파일로 분리</p>
+      <img src="/logo512.png"></img>
+      <p>소스호출하기 (public은  루트디렉토리로 / 하면 됩니다.)</p>
+      <p>src안의 넣은 소스는 node의 컴파일과정을 거치므로</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Head from './layout/Hd'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <Head></Head>
+    <App />
+  </>
+
+);
+
+
